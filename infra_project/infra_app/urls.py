291b854b2bdc32e2fc92f022673629e2f18423c5
Nemets87 <bonustime161@yandex.ru>
2023-02-18 21:27:19 +0300

    add tests powersss

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from . import views
+from infra_project.infra_app import views
 
 app_name = 'infra_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/second_page', views.second_page, name='second_page'),
+    path('second_page', views.second_page, name='second_page'),
 
 ]
