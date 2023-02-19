dc3032a09d293ee87bcb55408ab11c7413398e90
Nemets87 <bonustime161@yandex.ru>
2023-02-19 20:56:00 +0300

    add tests trues6

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -22,7 +22,6 @@ class StaticPagesURLTests(TestCase):
 
         response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
-
         
         
         
